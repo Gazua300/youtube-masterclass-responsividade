b2fb8f26325cffcda7ad20845a3acc1c93b3421f
Gazua300 <mesmo245@gmail.com>
2022-06-30 12:37:54 +0000

    alterações finalizadas

--- inicial/menu.js
+++ inicial/menu.js
@@ -1,8 +1,11 @@
-var shwo = true
+var show = true
 
 const menuSection = document.querySelector('.menu-section')
 const menuToggle = menuSection.querySelector('.menu-toggle')
 
 menuToggle.addEventListener('click', ()=>{
-    menuSection.classList.toggle('on')
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menuSection.classList.toggle('on', show)
+    show = !show
 })
